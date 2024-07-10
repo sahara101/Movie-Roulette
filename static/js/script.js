200d4e34fdea4389f084e776db5efb00b6ce3369
sahara101 <laurentiuriedl@yahoo.com>
2024-07-10 16:57:02 +0300

    Update and rename script.js to script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,10 @@
-// Main function which displays all movie data in the app
-// window.onload makes sure the function is run when the app starts up
 window.onload = document.getElementById('btn_watch').onclick = async function showMovieInHTML() {
-    // Get movie data from python
-    var py_movie = await eel.py_returnMovie()();
+    // Get movie data from Flask API
+    var response = await fetch('/random_movie');
+    var py_movie = await response.json();
 
     // After loading all data, show content of the app
     document.getElementById("section").classList.remove("hidden");
-
 
     // Changing data in HTML to movie details:
     // Title
@@ -17,7 +15,6 @@ window.onload = document.getElementById('btn_watch').onclick = async function sh
 
     // Director/s
     var directors_p = document.getElementById("directors");
-
     directors_p.innerHTML = "<span style=\"font-weight:700\">Directed by:</span>";
     py_movie["directors"].forEach(element => {
         directors_p.innerHTML += " " + element + ", ";
@@ -26,7 +23,6 @@ window.onload = document.getElementById('btn_watch').onclick = async function sh
 
     // Writer/s
     var writers_p = document.getElementById("writers");
-
     writers_p.innerHTML = "<span style=\"font-weight:700\">Written by:</span>";
     py_movie["writers"].forEach(element => {
         writers_p.innerHTML += " " + element + ", ";
@@ -35,20 +31,29 @@ window.onload = document.getElementById('btn_watch').onclick = async function sh
 
     // Actors
     var actors_p = document.getElementById("actors");
-
     actors_p.innerHTML = "<span style=\"font-weight:700\">Cast:</span>";
     py_movie["actors"].forEach(element => {
         actors_p.innerHTML += " " + element + ", ";
     });
     actors_p.innerHTML = actors_p.innerHTML.replace(/(\s+)?..$/, '');
 
+	// Description
+    document.getElementById("description").innerHTML = "<span style=\"font-weight:700\">Description:</span> " + py_movie["description"];
+
     // Poster
     document.getElementById("poster_img").src = py_movie["poster"];
 
     // BG Art
     document.getElementById("img_background").style.background = 'url(' + py_movie["background"] + ')';
-};
 
+    // Update TMDB, IMDB and Trakt links
+    document.getElementById("tmdb_link").href = py_movie["tmdb_url"];
+//    document.getElementById("tmdb_logo").src = "/logos/tmdb_logo.svg";
+    document.getElementById("trakt_link").href = py_movie["trakt_url"];
+//    document.getElementById("trakt_logo").src = "/logos/trakt_logo.svg";
+    document.getElementById("imdb_link").href = py_movie["imdb_url"];
+//    document.getElementById("imdb_logo").src = "/logos/imdb_logo.svg";
+};
 
 // This sections makes the "WATCH" button work...
 // Hides client prompt
@@ -58,8 +63,9 @@ function closeClientPrompt() {
 
 // Takes list of clients and display them as options for choosing where to watch content
 document.getElementById("btn_next_movie").addEventListener("click", async function() {
-    // Get list of clients from python
-    var clients = await eel.py_returnClients()();
+    // Get list of clients from Flask API
+    var response = await fetch('/clients');
+    var clients = await response.json();
 
     // Clear the list of clients
     list_of_clients.innerHTML = "";
@@ -73,6 +79,16 @@ document.getElementById("btn_next_movie").addEventListener("click", async functi
 });
 
 function playMovie(client) {
-    // Does what the name says...
-    eel.py_playMovie(client);
+    // Play movie on the specified client using Flask API
+    fetch('/play_movie/' + client)
+        .then(response => response.json())
+        .then(data => console.log(data)); // Optionally handle response
 }
+
+// This section makes the "START APPLE TV" button work...
+document.getElementById("btn_start_appletv").addEventListener("click", async function() {
+    // Start Apple TV using Flask API
+    fetch('/start_apple_tv')
+        .then(response => response.json())
+        .then(data => console.log(data)); // Optionally handle response
+});
