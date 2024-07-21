22302d6378b26404c5edb482fba8af5b685c29af
sahara101 <laurentiuriedl@yahoo.com>
2024-07-21 21:31:04 +0300

    added filter

--- randomPlexMovie.py
+++ randomPlexMovie.py
@@ -1,8 +1,8 @@
 import os
 import subprocess
+import random
 from flask import Flask, jsonify, render_template, send_from_directory, request
 from plexapi.server import PlexServer
-from random import choice
 from utils.fetch_movie_links import fetch_movie_links
 from utils.youtube_trailer import search_youtube_trailer
 
@@ -18,62 +18,124 @@ movies = plex.library.section(MOVIES_LIBRARY_NAME)
 
 app = Flask(__name__, static_folder='static', template_folder='web')
 
-# Global variable to store the chosen movie
+# Global variables
 chosen_movie = None
+filtered_movies = []
+last_movie = None
 
-# Routes
-@app.route('/')
-def index():
-    return render_template('index.html')
+global chosen_movie, filtered_movies, last_movie
+    try:
+        all_unwatched = movies.search(unwatched=True)
+        available_movies = [movie for movie in all_unwatched if movie != last_movie]
 
-@app.route('/style/<path:filename>')
-def style(filename):
-    return send_from_directory('static/style', filename)
+        if not available_movies:
+            available_movies = all_unwatched
 
-@app.route('/js/<path:filename>')
-def js(filename):
-    return send_from_directory('static/js', filename)
+        chosen_movie = random.choice(available_movies)
+        last_movie = chosen_movie
+        filtered_movies = all_unwatched
+        movie_data = get_movie_data(chosen_movie)
+        print("Movie data:", movie_data)  # Print the movie data
 
-@app.route('/logos/<path:filename>')
-def logos(filename):
-    return send_from_directory('static/logos', filename)
+        return movie_data
+    except Exception as e:
+        print(f"Error in random_movie: {str(e)}")
+        return jsonify({"error": str(e)}), 500
 
-# REST API endpoints
-@app.route('/random_movie')
-def random_movie():
-    '''Generates a list of unwatched movies. Randomly selects one to be displayed.
-    Pulls movie information and returns movie data for display'''
-    global chosen_movie
-    chosen_movie = choice(movies.search(unwatched=True))
-    chosen_movie_duration_hours = (chosen_movie.duration / (1000 * 60 * 60)) % 24
-    chosen_movie_duration_minutes = (chosen_movie.duration / (1000 * 60)) % 60
+@app.route('/filter_movies')
+def filter_movies():
+    global chosen_movie, filtered_movies, last_movie
+    genre = request.args.get('genre')
+    year = request.args.get('year')
+    rating = request.args.get('rating')
 
-    actors = [actor.tag for actor in chosen_movie.actors]
-    writers = [writer.tag for writer in chosen_movie.writers]
-    directors = [director.tag for director in chosen_movie.directors]
-    movie_description = chosen_movie.summary
+    filters = {'unwatched': True}
 
-    tmdb_url, trakt_url, imdb_url = fetch_movie_links(chosen_movie.title)
+    if genre:
+        filters['genre'] = genre
+    if year:
+        filters['year'] = int(year)
 
-    # Fetch trailer URL from YouTube
-    trailer_url = search_youtube_trailer(chosen_movie.title, chosen_movie.year)
+    filtered_movies = movies.search(**filters)
 
-    return jsonify({
-        "title": chosen_movie.title,
-        "year": chosen_movie.year,
-        "duration_hours": int(chosen_movie_duration_hours),
-        "duration_minutes": int(chosen_movie_duration_minutes),
-        "directors": directors,
-        "description": movie_description,
-        "writers": writers,
-        "actors": actors,
-        "poster": chosen_movie.posterUrl,
-        "background": chosen_movie.artUrl,
-        "tmdb_url": tmdb_url,
-        "trakt_url": trakt_url,
-        "imdb_url": imdb_url,
-        "trailer_url": trailer_url
-    })
+    if rating:
+        rating_floor = float(rating)
+        rating_ceiling = rating_floor + 0.9
+        filtered_movies = [movie for movie in filtered_movies if movie.rating and rating_floor <= float(movie.rating) < rating_ceiling]
+
+    if len(filtered_movies) == 0:
+        return jsonify({"error": "No movies found matching the filter"}), 404
+    elif len(filtered_movies) == 1:
+        chosen_movie = filtered_movies[0]
+        last_movie = chosen_movie
+        return get_movie_data(chosen_movie)
+    else:
+        available_movies = [movie for movie in filtered_movies if movie != last_movie]
+        if not available_movies:
+            available_movies = filtered_movies
+        chosen_movie = random.choice(available_movies)
+        last_movie = chosen_movie
+        return get_movie_data(chosen_movie)
+
+@app.route('/next_movie')
+def next_movie():
+    global chosen_movie, filtered_movies, last_movie
+
+    if not filtered_movies:  # If no filter is applied
+        return random_movie()
+
+    available_movies = [movie for movie in filtered_movies if movie != last_movie]
+    if not available_movies:
+        available_movies = filtered_movies
+
+    chosen_movie = random.choice(available_movies)
+    last_movie = chosen_movie
+    return get_movie_data(chosen_movie)
+
+def get_movie_data(movie):
+    try:
+        movie_duration_hours = (movie.duration / (1000 * 60 * 60)) % 24
+        movie_duration_minutes = (movie.duration / (1000 * 60)) % 60
+
+        actors = [actor.tag for actor in movie.actors]
+        writers = [writer.tag for writer in movie.writers]
+        directors = [director.tag for director in movie.directors]
+        genres = [genre.tag for genre in movie.genres]
+        movie_description = movie.summary
+
+        tmdb_url, trakt_url, imdb_url = fetch_movie_links(movie.title)
+        trailer_url = search_youtube_trailer(movie.title, movie.year)
+
+        return jsonify({
+            "title": movie.title,
+            "year": movie.year,
+            "duration_hours": int(movie_duration_hours),
+            "duration_minutes": int(movie_duration_minutes),
+            "directors": directors,
+            "description": movie_description,
+            "writers": writers,
+            "actors": actors,
+            "genres": genres,
+            "poster": movie.posterUrl,
+            "background": movie.artUrl,
+            "tmdb_url": tmdb_url,
+            "trakt_url": trakt_url,
+            "imdb_url": imdb_url,
+            "trailer_url": trailer_url,
+            "rating": movie.rating
+        })
+    except Exception as e:
+        print(f"Error in get_movie_data: {str(e)}")
+        return jsonify({"error": str(e)}), 500
+
+@app.route('/get_genres')
+def get_genres():
+    all_genres = set()
+    for movie in movies.search(unwatched=True):
+        all_genres.update([genre.tag for genre in movie.genres])
+    genre_list = sorted(list(all_genres))
+    print("Available genres:", genre_list)  # Debugging line
+    return jsonify(genre_list)
 
 @app.route('/clients')
 def clients():
@@ -137,3 +199,6 @@ def turn_on_device(device):
             return jsonify({"status": f"Failed to control LG TV: {str(e)}"}), 500
     else:
         return jsonify({"status": "Unknown device"}), 400
+
+if __name__ == '__main__':
+    app.run(debug=True)
